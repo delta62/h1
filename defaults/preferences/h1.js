54150b90b4223506cd3878ead5769563ada1e817
Sam Singer Noedel <sam@samnoedel.com>
2014-01-05 11:16:08 -0800

    Defining preferences

--- defaults/preferences/h1.js
+++ defaults/preferences/h1.js
@@ -2,9 +2,10 @@
 pref('extensions.h1.referer.allow', false);
 
 // HTTP ETag
-pref('extensions.h1.etag.notify', false);
 pref('extensions.h1.etag.allow', false);
+pref('extensions.h1.etag.notify', false);
 
 // HTTP User-Agent
+pref('extensions.h1.ua.mangle', true);
 
 // Whitelist
