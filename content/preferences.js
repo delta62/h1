fbc38bb293124ab332b5eba44396be19ce3a3da3
Sam Singer Noedel <sam@samnoedel.com>
2014-01-09 19:58:28 -0800

    Added URI input validation

--- content/preferences.js
+++ content/preferences.js
@@ -1,11 +1,26 @@
 // Ensure namespace exists
 var H1 = H1 || { };
 
+const { classes: Cc, interfaces: Ci } = Components;
+
 H1.Preferences = {
 
-    unload: function(e) {
-        dump('unload\n');
-        H1.preferenceWindow = null;
+    add: function() {
+        let textbox = document.getElementById('h1-whitelist-uri');
+        let ioService = Cc['@mozilla.org/network/io-service;1']
+            .getService(Ci.nsIIOService);
+        try {
+            let uri = textbox.value;
+            if (!/:\/\//.test(uri)) {
+                uri = 'http://' + uri;
+            }
+            uri = ioService.newURI(uri, null, null);
+            dump('URI host: ' + uri.asciiHost + '\n');
+
+            textbox.value = '';
+        } catch (ex) {
+            dump('Invalid URI\n');
+        }
     }
 
 };
